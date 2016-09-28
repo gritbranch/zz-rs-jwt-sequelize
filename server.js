8407d74b5b091afa0bf8057ce5b92bc8e9af38eb
ryansalvador <ryansalvador@gmail.com>
2016-09-28 17:18:03 +0800

    03: Basic JWT

--- server.js
+++ server.js
@@ -123,14 +123,15 @@ var findAll = function(req, res) {
 };
 
 var find = function(req, res, next, id) {
-    console.log('id => ' + id);
     User.find({where: {id: id}}).then(function(user){
         if(!user) {
             return next(new Error('Failed to load user ' + id));
         } else {
             req.user = user;
-            return next();            
+            //return next();
+            next(); return user; // to avoid promise was not returned error.            
         }
+        
     }).catch(function(err){
         return next(err);
     });
@@ -140,11 +141,29 @@ var show = function(req, res) {
     return res.json(req.user);
 };
 
+var update = function(req, res) {
+
+    var user = req.user;
+
+    user.updateAttributes({
+        username: req.body.username,
+        password: req.body.password
+    }).then(function(a){
+        return res.jsonp(a);
+    }).catch(function(err){
+        return res.render('error', {
+            error: err, 
+            status: 500
+        });
+    });
+
+};
+
 var destroy = function(req, res) {
 
     var user = req.user;
 
-    User.destroy().then(function(){
+    user.destroy().then(function(){
         return res.json(user);
     }).catch(function(err){
         return res.render('error', {
@@ -154,16 +173,86 @@ var destroy = function(req, res) {
     });
 };
 
+var createJwt = function (req, res) {
+    
+    //find user
+    User.find({where: {username: req.body.username}}).then(function(user){
+        if(!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+                // if user is found and password is right
+                var token = jwt.sign(user.dataValues, app.get('superSecret'), {
+                    expiresIn : 60*24 // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+
+                //{"success":true,"message":"Enjoy your token!","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwidXNlcm5hbWUiOiJyeWFuc2FsdmFkb3IzIiwicGFzc3dvcmQiOiJwYXNzMTIzIiwiYWRtaW4iOnRydWUsImNyZWF0ZWRBdCI6IjIwMTYtMDktMjdUMTE6NTQ6NDQuMDAwWiIsInVwZGF0ZWRBdCI6IjIwMTYtMDktMjdUMTE6NTQ6NDQuMDAwWiIsImlhdCI6MTQ3NTA1MDI2MiwiZXhwIjoxNDc1MDUxNzAyfQ.5gLxk-UZU19kojctRku7FUyX_nrbI1siCXWcBmfoHd0"}
+            }
+        }
+
+    }).catch(function(err){
+        return err;
+    });
+};
+
+var authenticateJwt = function (req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+        if (err) {
+            return res.json({ success: false, message: 'Failed to authenticate token.' });    
+        } else {
+            // if everything is good, save to request for use in other routes
+            req.decoded = decoded;    
+            next();
+        }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({ 
+            success: false, 
+            message: 'No token provided.' 
+        });
+        
+    }
+};
+
+
 app.param('userId', find);
 
-//Routes
+//app.use(authenticateJwt);
+
+//API Routes
 app.route('/api/users')
     .get(findAll)
     .post(create);
+
 app.route('/api/users/:userId')
     .get(show)
-    .put(update) // not yet working
-    .delete(destroy); // not yet working
+    .put(authenticateJwt, update)
+    .delete(destroy);
+
+app.route('/api/createJwt')
+    .post(createJwt);
 
 // Finish with setting up the userId param
 // Note: the articles.article function will be called everytime then it will call the next function.
